bc02e85f55618dfa421e76370d28d0e861b0a7bf
Kimseangchamroeurn <kchamroeurn@paragoniu.edu.kh>
2025-12-06 19:58:22 +0700

    feature(todo-list):add navbar in todo-list content add input add buttonn search delete

--- src/container/sidebar/Sidebar.jsx
+++ src/container/sidebar/Sidebar.jsx
@@ -19,7 +19,7 @@ const Feature = (props) => {
   const name = props.name; // vea yor domlai pi icon 1 1 
   return (
     <div className='feature'>
-      <a href=""><Icon size={20} /></a>
+      <button><Icon size={20} /></button>
       <button>{name}</button>
     </div>
   );
